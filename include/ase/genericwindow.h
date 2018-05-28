7644587cbf32012af91e410eb000e8d69c4154c4
Sami <sami@invalid.com>
2018-05-28 13:12:57 +0300

    Add generic window

---
+++ include/ase/genericwindow.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "pointerbuttonevent.h"
+#include "pointermoveevent.h"
+#include "keyevent.h"
+#include "hoverevent.h"
+
+#include <btl/option.h>
+
+#include <functional>
+#include <string>
+
+namespace ase
+{
+    class GenericWindow
+    {
+    public:
+        GenericWindow(Vector2i size);
+
+        Vector2i getSize() const;
+        float getWidth() const;
+        float getHeight() const;
+
+        std::string const& getTitle() const;
+        void setTitle(std::string title);
+
+        void notifyClose();
+        void resize(Vector2i size);
+        void notifyRedraw();
+
+        void injectPointerButtonEvent(
+                unsigned int pointerIndex,
+                unsigned int buttonIndex,
+                Vector2f pos,
+                ButtonState buttonState);
+
+        void injectPointerMoveEvent(unsigned int pointerIndex, Vector2f pos);
+        void injectHoverEvent(unsigned int pointerIndex, Vector2f pos,
+                bool state);
+
+        void setCloseCallback(std::function<void()> func);
+        void setResizeCallback(std::function<void()> func);
+        void setRedrawCallback(std::function<void()> func);
+        void setButtonCallback(
+                std::function<void(PointerButtonEvent const&)> cb);
+        void setPointerCallback(
+                std::function<void(PointerMoveEvent const&)> cb);
+        void setKeyCallback(std::function<void(KeyEvent const&)> cb);
+        void setHoverCallback(std::function<void(HoverEvent const&)> cb);
+
+    private:
+        std::function<void()> closeCallback_;
+        std::function<void()> resizeCallback_;
+        std::function<void()> redrawCallback_;
+        std::function<void(PointerButtonEvent const& e)> buttonCallback_;
+        std::function<void(PointerMoveEvent const& e)> pointerCallback_;
+        std::function<void(KeyEvent const& e)> keyCallback_;
+        std::function<void(HoverEvent const& e)> hoverCallback_;
+
+        Vector2i size_;
+
+        btl::option<Vector2f> pointerPos_;
+        bool hover_ = false;
+
+        std::array<bool, 15> buttonPressedState_ = { {
+            false, false, false, false,
+            false, false, false, false,
+            false, false, false, false,
+            false, false, false
+        }};
+
+        std::string title_;
+    };
+} // namespace ase
+
---
+++ src/ase/genericwindow.cpp
@@ -0,0 +1,140 @@
+#include "genericwindow.h"
+
+#include <iostream>
+
+namespace ase
+{
+
+GenericWindow::GenericWindow(Vector2i size) :
+    size_(size)
+{
+}
+
+Vector2i GenericWindow::getSize() const
+{
+    return size_;
+}
+
+float GenericWindow::getWidth() const
+{
+    return size_[0];
+}
+
+float GenericWindow::getHeight() const
+{
+    return size_[1];
+}
+
+std::string const& GenericWindow::getTitle() const
+{
+    return title_;
+}
+
+void GenericWindow::setTitle(std::string title)
+{
+    title_ = title;
+}
+
+void GenericWindow::notifyClose()
+{
+    if (closeCallback_)
+        closeCallback_();
+}
+
+void GenericWindow::resize(Vector2i size)
+{
+    size_ = size;
+
+    if (resizeCallback_)
+        resizeCallback_();
+}
+
+void GenericWindow::notifyRedraw()
+{
+    if (redrawCallback_)
+        redrawCallback_();
+}
+
+void GenericWindow::injectPointerButtonEvent(
+        unsigned int pointerIndex,
+        unsigned int buttonIndex,
+        Vector2f pos,
+        ButtonState buttonState)
+{
+    buttonPressedState_[buttonIndex - 1] = true;
+
+    PointerButtonEvent event{
+                pointerIndex, buttonIndex, buttonState, pos
+    };
+
+    if (buttonCallback_)
+        buttonCallback_(event);
+}
+
+void GenericWindow::injectPointerMoveEvent(unsigned int /*pointerIndex*/,
+        Vector2f pos)
+{
+    Vector2f rel(0.0f, 0.0f);
+
+    if (pointerPos_.valid())
+        rel = pos - *pointerPos_;
+
+    pointerPos_ = btl::just(pos);
+
+    PointerMoveEvent event{
+        pos, rel, buttonPressedState_, hover_
+    };
+
+    if (pointerCallback_)
+        pointerCallback_(event);
+}
+
+void GenericWindow::injectHoverEvent(unsigned int pointerIndex, Vector2f pos,
+        bool state)
+{
+    hover_ = state;
+
+    injectPointerMoveEvent(pointerIndex, pos);
+
+    if (hoverCallback_)
+        hoverCallback_(HoverEvent{ state });
+}
+
+void GenericWindow::setCloseCallback(std::function<void()> func)
+{
+    closeCallback_ = std::move(func);
+}
+
+void GenericWindow::setResizeCallback(std::function<void()> func)
+{
+    resizeCallback_ = std::move(func);
+}
+
+void GenericWindow::setRedrawCallback(std::function<void()> func)
+{
+    redrawCallback_ = std::move(func);
+}
+
+void GenericWindow::setButtonCallback(
+        std::function<void(PointerButtonEvent const& e)> cb)
+{
+    buttonCallback_ = std::move(cb);
+}
+
+void GenericWindow::setPointerCallback(
+        std::function<void(PointerMoveEvent const&)> cb)
+{
+    pointerCallback_ = std::move(cb);
+}
+
+void GenericWindow::setKeyCallback(std::function<void(KeyEvent const&)> cb)
+{
+    keyCallback_ = std::move(cb);
+}
+
+void GenericWindow::setHoverCallback(std::function<void(HoverEvent const&)> cb)
+{
+    hoverCallback_ = std::move(cb);
+}
+} // namespace ase
+
