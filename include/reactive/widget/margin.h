cf973633f4d974a0152d2b7fd7cd183b7ab5bd21
Sami <sami@invalid.com>
2018-07-26 23:46:55 +0300

    Fix margin transform

--- include/reactive/widget/margin.h
+++ include/reactive/widget/margin.h
@@ -13,59 +13,59 @@
 
 #include <iostream>
 
-namespace reactive
+namespace reactive::widget
 {
-    namespace widget
+    template <typename TSignalAmount>
+    auto growSize(TSignalAmount amount)
     {
-        template <typename TSignalAmount>
-        auto growSize(TSignalAmount amount)
-        {
-            return makeWidgetMap<ObbTag>(
-                [](avg::Obb const& obb, float amount) -> avg::Obb
-                {
-                    auto size = obb.getSize();
-                    auto newSize = avg::Vector2f(
-                        size[0] + 2.0f * amount,
-                        size[1] + 2.0f * amount
-                        );
-
-                    auto translate = avg::Transform()
-                        .translate(-amount, -amount);
-
-                    return obb.getTransform() * translate * avg::Obb(newSize);
-                },
-                std::move(amount)
-                );
-        }
-
-        template <typename TSignalAmount, typename = std::enable_if_t<
-            IsSignalType<TSignalAmount, float>::value
-            >
-        >
-        auto margin(TSignalAmount amount)
-        {
-            auto a = signal::share(std::move(amount));
-            auto aNeg = signal::map([](float f)
-                    {
-                        return -f;
-                    }, a);
-
-            auto factoryGrowSizeHint = [a](WidgetFactory factory)
-                -> WidgetFactory
+        return makeWidgetMap<ObbTag>(
+            [](avg::Obb const& obb, float amount) -> avg::Obb
             {
-                auto hint = signal::map(BTL_FN(growSizeHint),
-                        factory.getSizeHint(),
-                        a);
+                auto size = obb.getSize();
+                auto newSize = avg::Vector2f(
+                    size[0] + 2.0f * amount,
+                    size[1] + 2.0f * amount
+                    );
 
-                return std::move(factory)
-                    .setSizeHint(std::move(hint));
-            };
+                return avg::Obb(newSize);
+            },
+            std::move(amount)
+            );
+    }
 
-            return preMapFactory(growSize(std::move(aNeg)))
-                >> mapFactoryWidget(growSize(a))
-                >> std::move(factoryGrowSizeHint)
-                ;
-        }
-    } // widget
-} // reactive
+    template <typename TSignalAmount, typename = std::enable_if_t<
+        IsSignalType<TSignalAmount, float>::value
+        >
+    >
+    auto margin(TSignalAmount amount)
+    {
+        auto a = signal::share(std::move(amount));
+        auto aNeg = signal::map([](float f)
+                {
+                    return -f;
+                }, a);
+
+        auto t = signal::map([](float amount)
+                {
+                    return avg::translate(amount, amount);
+                }, a);
+
+        auto factoryGrowSizeHint = [a](WidgetFactory factory)
+            -> WidgetFactory
+        {
+            auto hint = signal::map(BTL_FN(growSizeHint),
+                    factory.getSizeHint(),
+                    a);
+
+            return std::move(factory)
+                .setSizeHint(std::move(hint));
+        };
+
+        return preMapFactory(growSize(std::move(aNeg)))
+            >> mapFactoryWidget(transform(std::move(t)))
+            >> mapFactoryWidget(growSize(a))
+            >> std::move(factoryGrowSizeHint)
+            ;
+    }
+} // reactive::widget
 
