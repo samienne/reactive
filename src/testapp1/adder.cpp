31d386a29b36b65a60b32221a101695cd4ac2936
Sami <sami@invalid.com>
2019-03-31 15:11:26 +0300

    Improve update functionality inside adder

--- src/testapp1/adder.cpp
+++ src/testapp1/adder.cpp
@@ -18,7 +18,9 @@ using namespace reactive;
 
 namespace
 {
-    WidgetFactory itemEntry(std::function<void(std::string text)> onEnter)
+    WidgetFactory itemEntry(
+            signal::InputHandle<std::string> outHandle,
+            std::function<void(std::string text)> onEnter)
     {
         auto textState = signal::input(widget::TextEditState(""));
         auto handle = textState.handle;
@@ -33,8 +35,15 @@ namespace
                 textState.signal
                 ));
 
+        auto state = signal::tee(textState.signal,
+                [](widget::TextEditState const& state)
+                {
+                    return state.text;
+                },
+                outHandle);
+
         return hbox({
-                textEdit(handle, textState.signal)
+                textEdit(handle, std::move(state))
                     .onEnter(onEnterSignal),
                 widget::label(signal::constant<std::string>("Add"))
                     | widget::frame()
@@ -42,15 +51,15 @@ namespace
             });
     }
 
-    WidgetFactory button(std::string label, std::function<void()> cb)
+    template <typename TFunc, typename U>
+    WidgetFactory button(std::string label, Signal<TFunc, U> cb)
     {
         return widget::label(signal::constant(std::move(label)))
                         | widget::frame()
-                        | widget::onClick(0, [cb=std::move(cb)]
-                            (ClickEvent const&)
-                            {
-                                cb();
-                            })
+                        | widget::onClick(0,
+                                signal::cast<std::function<void()>>(
+                                    std::move(cb)
+                                    ))
                         ;
     }
 } // anonymous namespace
@@ -63,35 +72,40 @@ reactive::WidgetFactory adder()
     items.pushBack("test 2");
     items.pushBack("test 3");
 
+    auto textInput = signal::input<std::string>("");
+
     auto widgets = signal::dataBind<std::string>(
             items,
-            [items]
+            [items, textInputSignal=std::move(textInput.signal)]
             (Signal<std::string> value, size_t id) mutable -> WidgetFactory
             {
                 return hbox({
-                    button("->", [items, id]() mutable
+                    button("->", signal::mapFunction([items, id]
+                        (std::string str) mutable
                         {
                             auto i = items.findId(id);
                             if (i != items.end())
                             {
-                                items.update(i, "updated");
+                                items.update(i, std::move(str));
                             }
-                        })
+                        },
+                        textInputSignal.clone()
+                        ))
                     ,
                     widget::label(std::move(value))
                     ,
                     hfiller()
                     ,
-                    button("x", [id, items]() mutable
+                    button("x", signal::constant([id, items]() mutable
                         {
                             items.eraseWithId(id);
-                        })
+                        }))
                     });
             });
 
     return vbox({
             vbox(std::move(widgets)),
-            itemEntry([items](std::string text) mutable
+            itemEntry(textInput.handle, [items](std::string text) mutable
                 {
                     items.pushBack(text);
                 })
