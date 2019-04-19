40ecd4334070857d22c6ae1818cda3c369888a3a
Sami <sami@invalid.com>
2019-04-19 21:41:21 +0300

    Add rounding option to widget::frame

--- include/reactive/widget/frame.h
+++ include/reactive/widget/frame.h
@@ -17,9 +17,11 @@
 
 namespace reactive::widget
 {
-    inline auto frame()
+    template <typename T>
+    auto frame(Signal<float, T> cornerRadius)
     {
-        auto f = [](avg::Vector2f size, widget::Theme const& theme)
+        auto f = [](avg::Vector2f size, widget::Theme const& theme,
+                float cornerRadius)
             -> avg::Drawing
         {
             auto pen = avg::Pen(avg::Brush(theme.getBackgroundHighlight()),
@@ -28,8 +30,8 @@ namespace reactive::widget
             auto brush = avg::Brush(theme.getBackground());
 
             auto shape =  makeShape(
-                    //makeRect(size[0] - 5.0f, size[1] - 5.0f),
-                    makeRoundedRect(size[0] - 5.0f, size[1] - 5.0f, 10.0f),
+                    makeRoundedRect(size[0] - 5.0f, size[1] - 5.0f,
+                        cornerRadius),
                     btl::none,
                     btl::just(pen));
 
@@ -42,7 +44,13 @@ namespace reactive::widget
         return
             margin(signal::constant(5.0f))
             >> mapFactoryWidget(onDrawBehind<SizeTag, ThemeTag>(
-                        std::move(f)))
+                        std::move(f), std::move(cornerRadius)))
             ;
     }
+
+    inline auto frame()
+    {
+        return frame(signal::constant(10.0f));
+    }
 } // namespace reactive::widget
+
