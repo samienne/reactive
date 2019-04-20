5fdf7eb1ac269c2a718fdfe35ca775a5a6fdc3c1
Sami <sami@invalid.com>
2019-04-20 13:25:48 +0300

    Remove commented out code

--- include/reactive/widget/button.h
+++ include/reactive/widget/button.h
@@ -12,16 +12,5 @@ namespace reactive::widget
 
     REACTIVE_EXPORT WidgetFactory button(std::string label,
             Signal<std::function<void()>> onClick);
-
-    /*
-    template <typename T, typename U, typename = std::enable_if<
-        std::is_convertible_v<T, std::function<void()>>
-        >>
-    WidgetFactory button(std::string label, Signal<T, U> onClick)
-    {
-        return button(std::move(label), signal::cast<std::function<void()>>(
-                    std::move(onClick)));
-    }
-    */
 } // namespace reactive::widget
 
