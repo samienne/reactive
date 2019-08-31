3ed9d97befa3a3217a34f87c1468a551e5bd669c
Sami <sami@invalid.com>
2019-08-31 13:16:40 +0300

    refactor scrollview a bit

--- src/reactive/widget/scrollview.cpp
+++ src/reactive/widget/scrollview.cpp
@@ -51,14 +51,6 @@ WidgetFactory scrollView(WidgetFactory f)
     auto x = signal::input(0.5f);
     auto y = signal::input(0.5f);
 
-    auto t = signal::map([](float x, float y,
-                avg::Vector2f contentSize, avg::Vector2f viewSize)
-            {
-                return avg::translate(
-                        x * -(contentSize[0] - viewSize[0]),
-                        (1.0f - y) * (contentSize[1] - viewSize[1]));
-            }, x.signal, y.signal, contentSize.signal, viewSize.signal);
-
     auto hHandleSize = signal::map([](avg::Vector2f contentSize,
                 avg::Vector2f viewSize)
             {
@@ -80,66 +72,73 @@ WidgetFactory scrollView(WidgetFactory f)
     auto dragOffset = signal::input(avg::Vector2f());
     auto scrollPos = signal::input<btl::option<avg::Vector2f>>(btl::none);
 
+    auto t = signal::map([](float x, float y,
+                avg::Vector2f contentSize, avg::Vector2f viewSize)
+            {
+                return avg::translate(
+                        x * -(contentSize[0] - viewSize[0]),
+                        (1.0f - y) * (contentSize[1] - viewSize[1]));
+            }, x.signal, y.signal, contentSize.signal, viewSize.signal);
+
     auto f2 = std::move(f)
         | trackSize(contentSize.handle)
         | transform(t.clone())
-        | onPointerDown(signal::mapFunction(
-                [dragOffsetHandle=dragOffset.handle,
-                scrollPosHandle=scrollPos.handle
-                ]
-                (avg::Vector2f contentSize, float x, float y,
-                 PointerButtonEvent const& e) mutable
-                {
-                    avg::Vector2f center(contentSize[0] / 2.0f,
-                            contentSize[1] / 2.0f);
-
-                    if (e.button == 1)
-                    {
-                        dragOffsetHandle.set(e.pos );//- center);
-                        scrollPosHandle.set(btl::just(avg::Vector2f(x, y)));
-                    }
-
-                    return EventResult::possible;
-                }, contentSize.signal, x.signal, y.signal))
-        | onPointerMove(signal::mapFunction(
-                [xHandle=x.handle, yHandle=y.handle]
-                (avg::Vector2f dragOffset, avg::Vector2f viewSize,
-                    avg::Vector2f contentSize,
-                    btl::option<avg::Vector2f> scrollPos,
-                    PointerMoveEvent const& e) mutable
-                {
-                    if (!scrollPos.valid())
-                        return EventResult::possible;
-
-                    float hLen = contentSize[0] - viewSize[0];
-                    float vLen = contentSize[1] - viewSize[1];
-
-                    float x = -(e.pos[0] - dragOffset[0]) / hLen + (*scrollPos)[0];
-                    float y = -(e.pos[1] - dragOffset[1]) / vLen + (*scrollPos)[1];
-
-                    xHandle.set(std::max(0.0f, std::min(x, 1.0f)));
-                    yHandle.set(std::max(0.0f, std::min(y, 1.0f)));
-
-                    return EventResult::accept;
-                }, dragOffset.signal, viewSize.signal, contentSize.signal,
-                scrollPos.signal))
-        | onPointerUp([scrollPosHandle=scrollPos.handle]
-                (PointerButtonEvent const&) mutable
-                {
-                    scrollPosHandle.set(btl::none);
-                    return EventResult::reject;
-                });
         ;
 
     auto view = bin(std::move(f2), hintMap,
-            [contentSize=std::move(contentSize.signal),
-            viewSize=std::move(viewSize.signal)]
+            [contentSize=contentSize.signal,
+            viewSize=viewSize.signal]
             (Signal<SizeHint> sizeHint)
             {
                 return obbMap(std::move(sizeHint), btl::clone(contentSize),
                         btl::clone(viewSize));
             })
             | trackSize(viewSize.handle)
+            | makeWidgetMap()
+                .map(onPointerDown(signal::mapFunction(
+                    [dragOffsetHandle=dragOffset.handle,
+                    scrollPosHandle=scrollPos.handle
+                    ]
+                    (float x, float y,
+                    PointerButtonEvent const& e) mutable
+                    {
+                        if (e.button == 1)
+                        {
+                            dragOffsetHandle.set(e.pos );
+                            scrollPosHandle.set(btl::just(avg::Vector2f(x, y)));
+                        }
+
+                        return EventResult::possible;
+                    }, x.signal, y.signal)))
+            .map(onPointerMove(signal::mapFunction(
+                    [xHandle=x.handle, yHandle=y.handle]
+                    (avg::Vector2f dragOffset, avg::Vector2f viewSize,
+                        avg::Vector2f contentSize,
+                        btl::option<avg::Vector2f> scrollPos,
+                        PointerMoveEvent const& e) mutable
+                    {
+                        if (!scrollPos.valid())
+                            return EventResult::possible;
+
+                        float hLen = contentSize[0] - viewSize[0];
+                        float vLen = contentSize[1] - viewSize[1];
+
+                        float x = -(e.pos[0] - dragOffset[0]) / hLen + (*scrollPos)[0];
+                        float y = -(e.pos[1] - dragOffset[1]) / vLen + (*scrollPos)[1];
+
+                        xHandle.set(std::max(0.0f, std::min(x, 1.0f)));
+                        yHandle.set(std::max(0.0f, std::min(y, 1.0f)));
+
+                        return EventResult::accept;
+                    }, dragOffset.signal, viewSize.signal, contentSize.signal,
+                    scrollPos.signal)))
+            .map(onPointerUp([scrollPosHandle=scrollPos.handle]
+                    (PointerButtonEvent const&) mutable
+                    {
+                        scrollPosHandle.set(btl::none);
+                        return EventResult::reject;
+                    }))
+
             | widget::frame()
         ;
 
