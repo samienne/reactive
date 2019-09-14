d7ecb6c442ffd50cf52482fff51df088566139cd
Sami <sami@invalid.com>
2019-09-14 14:18:03 +0300

    Remove dead code

--- src/reactive/widget/scrollview.cpp
+++ src/reactive/widget/scrollview.cpp
@@ -13,38 +13,6 @@
 namespace reactive::widget
 {
 
-namespace
-{
-    SizeHint hintMap(SizeHint /*hint*/)
-    {
-        return simpleSizeHint(
-                {{100, 400, 10000}},
-                {{100, 800, 10000}}
-                );
-    }
-
-    Signal<avg::Obb> obbMap(Signal<SizeHint> hint,
-            Signal<avg::Vector2f> contentSize,
-            Signal<avg::Vector2f> viewSize)
-    {
-        return signal::map(
-                [](SizeHint hint, avg::Vector2f contentSize,
-                    avg::Vector2f viewSize)
-                {
-                    float w = hint.getWidth()[1];
-                    float h = hint.getHeightForWidth(w)[1];
-
-                    float offY = contentSize[1] - viewSize[1];
-
-                    return avg::translate(0.0f, -offY)
-                        * avg::Obb(avg::Vector2f(w, h));
-                },
-                std::move(hint),
-                std::move(contentSize),
-                std::move(viewSize));
-    }
-} // anonymous namespace
-
 WidgetFactory scrollView(WidgetFactory f)
 {
     auto contentSize = signal::input(avg::Vector2f());
